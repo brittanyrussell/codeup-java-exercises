e2270523fd9434ed633602aa4ff4aabb9ba62c62
brittanyrussell <brittanyrussell11032gmail.com>
2018-09-17 11:51:29 -0500

    completed syntax exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+        int x = 10;
+        int y = 2;
+        x = x / y;
+        y = y - x;
+        System.out.println(y);
+        System.out.println(x);
     }
 
 }
