62e2ad2cbaba20c3199aef62f3a08ac20a95ea49
brittanyrussell <brittanyrussell11032gmail.com>
2018-09-18 12:05:10 -0500

    exercise console io without comments

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -2,56 +2,58 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
 
-//        CALCULATE PI
+//       PRINT PI WITH 2 DECIMALS
         double pi = 3.14159;
-        System.out.format("The value of pi is approximately %.2f.", pi);
+        System.out.format("The value of pi is approximately %.2f.\n", pi);
 
 //        ENTER A NUMBER
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.print(" Enter a number: ");
-//        String userInput = scanner.next();
-//
-//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a interger: ");
+        int val1 = scanner.nextInt();
+
+        System.out.format("You entered %d\n", val1);
 
 
 
 //          ENTER 3 WORDS
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.print(" Enter 3 words: ");
-//        String userInput = scanner.nextLine();
-//
-//        System.out.println("You entered: --> \"" + userInput + "\" <--");
-//        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+
+        System.out.print("Enter 3 words: ");
+        String s1 = scanner1.next();
+        String s2 = scanner1.next();
+        String s3 = scanner1.next();
+
+        System.out.format("%s%n%s%n%s%n", s1, s2, s3);
 
 
 //        ENTER A SENTENCE
-//        System.out.print(" Enter a sentence: ");
-//        String userInput = scanner.nextLine();
-//
-//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner2.nextLine();
+
+        System.out.println(sentence);
 
 
 
 
 //        CALCULATE AREA
-//        Scanner userInput = new Scanner(System.in);
-//        System.out.println(" Enter length and width" + "(Press Enter after each):");
-//        double n1, n2, n3;
-//        n1 = userInput.nextDouble();
-//        n2 = userInput.nextDouble();
-//        n3 = n1 * n2;
-//        System.out.println("Area = " + n3);
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter length and width" + "(Press Enter after each):");
+        double a1, a2, a3;
+        a1 = userInput.nextDouble();
+        a2 = userInput.nextDouble();
+        a3 = a1 * a2;
+        System.out.println("Area = " + a3);
 
 
 //        CALCULATE PERIMETER
-//        Scanner userInput = new Scanner(System.in);
-//        System.out.println(" Enter length and width" + "(Press Enter after each):");
-//        double n1, n2, n3;
-//        n1 = userInput.nextDouble();
-//        n2 = userInput.nextDouble();
-//        n3 = 2 * n1 + 2 * n2;
-//        System.out.println("Perimeter = " + n3);
+        Scanner userInput2 = new Scanner(System.in);
+        System.out.println("Enter length and width" + "(Press Enter after each):");
+        double p1, p2, p3;
+        p1 = userInput2.nextDouble();
+        p2 = userInput2.nextDouble();
+        p3 = 2 * p1 + 2 * p2;
+        System.out.println("Perimeter = " + p3);
     }
 }
