6ecd0ee7d9685013c232d7960f6cda54487b92e5
brittanyrussell <brittanyrussell11032gmail.com>
2018-09-18 14:45:27 -0500

    do while 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,10 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 2;
-        while (i <= 100) {
+        int i = 100;
+        while (i >= 5) {
             System.out.println(i);
-            i++;
+            i--;
         }
 
 
