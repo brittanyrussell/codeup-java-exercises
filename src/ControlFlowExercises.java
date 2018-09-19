f15801c0cef08ce5fe7346870e676c9198364be5
brittanyrussell <brittanyrussell11032gmail.com>
2018-09-19 09:18:47 -0500

    fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,22 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        for (int i  = 100; i >= 5;) {
-               int minus =  i-5;
-               i--;
-            System.out.println(minus);
+        for (int i = 1; i <= 100; i++) {
+            boolean fizzOrBuzz = false;
+            if (i % 3 == 0) {
+                System.out.print("Fizz");
+                fizzOrBuzz = true;
+            }
+            if (i % 5 == 0) {
+                System.out.print("Buzz");
+                fizzOrBuzz = true;
+            }
 
+            if (fizzOrBuzz) {
+                System.out.println();
+            } else {
+                System.out.println(String.valueOf(i));
+            }
         }
 
 
