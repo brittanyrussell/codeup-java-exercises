a7e7c942e342ffdbadd33f8239ab21c9d6ec040a
brittanyrussell <brittanyrussell11032gmail.com>
2018-09-19 19:34:15 -0500

    method exercise 1. no bonus

---
+++ src/MethodExercises.java
@@ -0,0 +1,29 @@
+public class MethodExercises {
+
+    public static void addition(int i, int j) {
+        System.out.println(i + j);
+    }
+
+    public static void subtraction(int i, int j) {
+        System.out.println(i - j);
+    }
+
+    public static void multiplication(int i, int j) {
+        System.out.println(i * j);
+    }
+
+    public static void division(int i, int j) {
+        System.out.println(i / j);
+    }
+    public static void modulus(int i, int j) {
+        System.out.println(i%j);
+    }
+    public static void main(String[] args) {
+        addition(3, 4);
+        subtraction(7, 4);
+        multiplication(5,4);
+        division(8, 2);
+        modulus(9,2);
+
+    }
+}
