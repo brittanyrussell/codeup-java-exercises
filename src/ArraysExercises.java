01763fb03b1bc4691fabc8cd601806c042b3cf5e
brittanyrussell <brittanyrussell11032gmail.com>
2018-09-24 14:57:38 -0500

    array exercise problem 1

---
+++ src/ArraysExercises.java
@@ -0,0 +1,37 @@
+public class ArraysExercises {
+    public static void main(String[] args) {
+
+
+        Person person1 = new Person("Robert");
+        Person person2 = new Person("Mary");
+        Person person3 = new Person("Brittany");
+        Person person4 = new Person("Katy");
+
+
+        Person[] people = {person1, person2, person3, person4};
+
+        Person person5 = new Person("Amanda");
+        people = addPerson(people, person5);
+
+        for (Person person : people) {
+
+            System.out.println(person.getName());
+        }
+
+    }
+       public static Person[] addPerson(Person[] people, Person anotherPerson) {
+           Person[] newPeople = new Person[people.length + 1];
+           newPeople[0] = people[0];
+           newPeople[1] = people[1];
+           newPeople[2] = people[2];
+           newPeople[3] = people[3];
+           newPeople[4] = anotherPerson;
+
+           return newPeople;
+
+
+        }
+
+
+
+}
--- src/pushUp.java
+++ src/pushUp.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class pushUp {
     public static void main(String[] args) {
 
+
+
 //        int[] numbers = {1, 2, 3, 4, 5};
 //        for (int n : numbers) {
 //            System.out.println(n);
