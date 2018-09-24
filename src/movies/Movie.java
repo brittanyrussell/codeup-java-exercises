90e3a20f5ec5e003286f5550c6aa8d74a75f3e56
brittanyrussell <brittanyrussell11032gmail.com>
2018-09-24 16:59:25 -0500

    movie appliation in array exercise

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,4 +1,32 @@
 package movies;
 
 public class Movie {
+    private String name;
+    private String category;
+
+
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(String category) {
+
+        this.category = category;
+    }
+
+    public String getCategory() {
+
+        return category;
+    }
+
+    public String getName() {
+
+        return name;
+
+    }
 }
