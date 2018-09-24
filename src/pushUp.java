d8dad4a888f64347ee5736513810a09b5fd1d932
brittanyrussell <brittanyrussell11032gmail.com>
2018-09-24 11:13:29 -0500

    pushup and lesson work

--- src/pushUp.java
+++ src/pushUp.java
@@ -2,13 +2,55 @@ import java.util.Scanner;
 public class pushUp {
     public static void main(String[] args) {
 
-        for (int i = 7; i >= 1; i--) {
-            System.out.println(i);
-            if (i == 1) {
-                System.out.println("Blastoff!!");
-            }
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        for (int n : numbers) {
+//            System.out.println(n);
+//        }
+//        int[] numbers = {1,2,3,4,5,6,7,8,9};
+//        for (int num:numbers) System.out.print(num + " ");
+//        System.out.println();
+//        for (int num:reverse(numbers)) System.out.print(num + " ");
+//
+//        int[][] matrix = {
+//                {1, 2, 3},
+//                {4, 5, 6},
+//                {7, 8, 9}
+//        };
+//
+//        for (int[] row : matrix) {
+//            System.out.println("+---+---+---+");
+//
+//            System.out.print("| ");
+//
+//            for (int n : row) {
+//                System.out.print(n + " | ");
+//            }
+//
+//            System.out.println();
+//        }
+//
+//        System.out.println("+---+---+---+");
+//
+//        int[][] matrix = {
+//                {1, 2, 3},
+//                {4, 5, 6},
+//                {7, 8, 9}
+//        };
+//
+//// access the first element in the second row
+//        System.out.println(matrix[1][0]); // 4
+//// the last element in the first row
+//        System.out.println(matrix[0][2]); // 3
+//// the first element in the last row
+//        System.out.println(matrix[2][0]); // 7
 
-        }
+//        for (int i = 7; i >= 1; i--) {
+//            System.out.println(i);
+//            if (i == 1) {
+//                System.out.println("Blastoff!!");
+//            }
+//
+//        }
 
 
 //      byte byteVar = 100;
@@ -39,4 +81,16 @@ public class pushUp {
 //        System.out.format(messageTemplate, className, dayOfWeek);
 
     }
+//    public static int[] reverse(int[] nums) {
+//        int[] backwards = new int[nums.length];
+//        for (int i =0;i<nums.length;i++) {
+//            //backwards.unshift(nums[i])
+//            for (int num:backwards)
+//                System.out.print(num + " ");
+//            System.out.println();
+//            backwards[backwards.length - i - 1] = nums[i];
+//        }
+//        return backwards;
+//    }
+
 }
