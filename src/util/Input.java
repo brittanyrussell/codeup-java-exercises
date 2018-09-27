d8b65f41554a1d427b6136d322141ff40e69f746
brittanyrussell <brittanyrussell11032gmail.com>
2018-09-27 18:06:55 -0500

    completed exceptions exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -4,6 +4,7 @@ public class Input {
     private Scanner scan;
 
     // constructor runs whenever we see "new Input()"
+
     public Input() {
         scan = new Scanner(System.in).useDelimiter("\n");
     }
@@ -18,8 +19,19 @@ public class Input {
     }
 
     public int getInt() {
-        return scan.nextInt();
-    }
+
+        String userInput = this.getString("Give me a number");
+        int number;
+
+        try {
+            number = Integer.valueOf(userInput);
+        } catch (NumberFormatException e) {
+            System.out.println("This is not a valid number");
+            number = getInt();
+        }
+        return number;
+//        return scan.nextInt();
+}
 
     public int getInt(int min, int max) {
         System.out.println("Please input an integer between " + min + " and " + max);
@@ -27,17 +39,70 @@ public class Input {
     }
 
     public double getDouble() {
-        return scan.nextDouble();
+        String userInput = this.getString("Give me a decimal number");
+        double number;
+
+        try {
+            number = Double.valueOf(userInput);
+        } catch (NumberFormatException e) {
+            System.out.println("This is not a valid decimal number");
+            number = getDouble();
+        }
+        return number;
+//        return scan.nextDouble();
     }
 
     public double getDouble(double min, double max) {
+
         System.out.println("Please input an integer between " + min + " and " + max);
         return getDouble();
     }
 
     public boolean yesNo() {
+
         System.out.println("Please input yes or y to continue");
         String userInput = getString();
         return userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes");
     }
+
+    public int getBinary() {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a binary number:");
+        int n= input.nextInt();
+        int decimal=0,p=0;
+        while(n!=0) {
+            decimal +=((n%10)*Math.pow(2,p));
+            n=n/10;
+            p++;
+        }
+        return decimal;
+
+    }
+
+//    public int getHex() {
+//
+//        int number;
+//
+//        void getValue()
+//        {
+//            Scanner sc = new Scanner(System.in);
+//            System.out.println("Please enter hexadecimal to convert: ");
+//            number = Integer.parseInt(sc.nextLine(), 16);
+//            sc.close();
+//        }
+//
+//        void toConvert()
+//        {
+//            String decimal = Integer.toString(number);
+//            System.out.println("The Decimal value is : " + decimal);
+//        }
+//        public static void main(String[] args)
+//        {
+//            HexaToDecimal htd = new HexaToDecimal();
+//            htd.getValue();
+//            htd.toConvert();
+//        }
+//    }
+
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,25 +1,70 @@
 package util;
 
+import java.util.Scanner;
+
 public class InputTest {
     public static void main(String[] args) {
+//        Input input = new Input();
+//        boolean confirm = true;
+//        while (confirm) {
+//            System.out.println(input.getInt());
+//            System.out.println("Continue working");
+//
+//        System.out.println("yes or no");
+//        Scanner input2 = new Scanner(System.in);
+//        String answer = input2.nextLine();
+//        if (!answer.toLowerCase().contains("y")) {
+//            confirm = false;
+//        }
+//        }
+//        Input input = new Input();
+//        boolean confirm = true;
+//        while (confirm) {
+//            System.out.println(input.getDouble());
+//            System.out.println("Continue working");
+//
+//        System.out.println("yes or no");
+//        Scanner input2 = new Scanner(System.in);
+//        String answer = input2.nextLine();
+//        if (!answer.toLowerCase().contains("y")) {
+//            confirm = false;
+//        }
+//        }
+
         Input input = new Input();
-        System.out.println("Would you like to input a double?");
+        boolean confirm = true;
+        while (confirm) {
+            System.out.println(input.getBinary());
+            System.out.println("Continue working");
 
-        if(input.yesNo()) {
-            double someDouble = input.getDouble();
-            System.out.println("You entered: " + someDouble);
-
-        } else {
-            System.out.println("Ok.");
+        System.out.println("yes or no");
+        Scanner input2 = new Scanner(System.in);
+        String answer = input2.nextLine();
+        if (!answer.toLowerCase().contains("y")) {
+            confirm = false;
+        }
         }
 
-        System.out.println("Would you like to input an integer?");
-        if(input.yesNo()) {
-            int userInt = input.getInt();
-            System.out.println("You entered: " + userInt);
-        } else {
-            System.out.println("ok");
-        }
+
+
+//        Input input = new Input();
+//        System.out.println("Would you like to input a double?");
+//
+//        if(input.yesNo()) {
+//            double someDouble = input.getDouble();
+//            System.out.println("You entered: " + someDouble);
+//
+//        } else {
+//            System.out.println("Ok.");
+//        }
+//
+//        System.out.println("Would you like to input an integer?");
+//        if(input.yesNo()) {
+//            int userInt = input.getInt();
+//            System.out.println("You entered: " + userInt);
+//        } else {
+//            System.out.println("ok");
+//        }
 
     }
 }
